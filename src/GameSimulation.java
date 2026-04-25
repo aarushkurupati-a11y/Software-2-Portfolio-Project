9b3a95394d31cf58bd2aad0e2756683a5f659bd2
aarushkurupati-a11y <aarushkurupati@gmail.com>
2026-04-24 22:22:31 -0400

    Update GameSimulation.java

--- src/GameSimulation.java
+++ src/GameSimulation.java
@@ -1,3 +1,14 @@
 public class GameSimulation {
+    public static void main(String[] args) {
+        VolleyballRotation team = new VolleyballRotation1L();
 
+        team.addPlayer("Setter");
+        team.addPlayer("Libero");
+        team.addPlayer("Outside");
+
+        System.out.println(team.currentServer());
+
+        team.rotate();
+        System.out.println(team.currentServer());
+    }
 }
